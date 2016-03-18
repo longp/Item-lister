9a1a700dd347dedf2317eab5128836cc057d697b
long phan <longphan@longs-MacBook-Pro.local>
2016-03-18 19:53:40 -0400

    added res.sendfile to send html file

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
+var  app = express();
+
+
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
+
+
+app.listen(3000)
