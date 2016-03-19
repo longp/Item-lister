19a6da4be0a247b008151b44bf38029fb5f88157
Long Phan <lphan1989@gmail.com>
2016-03-18 20:43:04 -0400

    added bootstrap,angular jquery cdns and added/fixed home route in app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 var  app = express();
 
+app.use(express.static(_dirname + "public"));
+app.use(bodyParser.json());
+
 
 
 app.get("/", function (req, res) {
