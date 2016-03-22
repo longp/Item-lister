ad0d96574c33a6baf2f79ecc9c65019f2f03ecde
long phan <longphan@longs-MacBook-Pro.local>
2016-03-22 01:18:11 -0400

    added mongoose conextion

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 var  app = express();
+var logger = require("morgan")
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+// mongoose coonnection
+var db = 'mongodb://localhost/itemLister'
+mongoose.connect(db);
 
+var User = require("./models/User.js");
+var Item = require("./models/Item.js");
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html")
